6254ebd59b61b4c6fef26109eb2bd326a8e3cbf5
Eduard Aleksandrov <eduard.ale@list.ru>
2024-04-28 22:57:02 +0300

    потоки

--- 22.pthread.c
+++ 22.pthread.c
@@ -1,26 +1,39 @@
+#define _GNU_SOURCE 
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
  
 void* some_work(void* arg) 
 {
-    for(int i = 0; i < 5; ++i) 
+    for(int i = 0; i < 2; ++i) 
     {
         puts(arg);
     }
+    printf("%d \n", gettid());
     return NULL;
 }
 int main(void) 
 { 
-    pthread_t thread[2];
+    pthread_t thread;
     // создаем потоки
-    pthread_create(&thread[0], NULL, some_work, "Hello World" );
-    pthread_create(&thread[1], NULL, some_work, "Hello METANIT.COM" );
+    int i = 0;
+    while(i<3)
+    {
+        pthread_create(&thread, NULL, some_work, "Hello World" );
+        i++;
+    }
+    // pthread_create(&thread[0], NULL, some_work, "Hello World" );
+    // pthread_create(&thread[1], NULL, some_work, "Hello METANIT.COM" );
  
     // pthread_exit(NULL);
-    for(int i = 0; i < 2; i++)
+    i=0;
+    while(i<3)
     {
-        pthread_join(thread[i], NULL);
+        pthread_join(thread, NULL);
+        i++;
     }
+        
+
     
     printf("End...\n");
     return 0;
